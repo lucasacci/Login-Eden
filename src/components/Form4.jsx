715d9bbd9afb7d7a8891b469c36c08395c431b04
Luciano <lucasacci@hotmail.com>
2022-10-26 00:26:27 -0300

    app done

--- src/components/Form4.jsx
+++ src/components/Form4.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import { useNavigate } from 'react-router-dom';
 
 export const Form4 = () => {
+
+  const navigate = useNavigate();
+
+  const onSubmit = () => {
+    navigate("/");
+  };
+
   return (
-    <div>Form4</div>
+    <motion.div
+    initial={{ width: 0 }}
+    animate={{ width: "100%" }}
+    exit={{ x: "100%" }}
+  >
+    <div className="container">
+      <div className='d-flex justify-content-center my-3'>
+          <i className="fa-solid fa-circle-check fs-1 "></i>
+      </div>
+      <h3 className="text-center">Congratulations, Eren!</h3>
+      <h6 className="text-secondary text-center my-1">
+        You have completed onboarding, you can start using the Eden.
+      </h6>
+    </div>
+    <div className="container d-flex justify-content-center my-3">
+      <div className='divColumn'>
+          <form onSubmit={onSubmit}>
+            <button className="btn my-3 text-light w-100" type='submit'>Create Workspace</button>
+          </form>
+        </div>
+      </div>
+  </motion.div>
   )
 }
--- src/components/ProgressBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ProgressBar = () => {
-  return (
-    <div className='text-center my-5'>ProgressBar</div>
-  )
-}
